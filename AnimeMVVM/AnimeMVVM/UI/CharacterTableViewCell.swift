5bb627aff10d143c529190db0f97c08bb39555b1
Colton Brenneman <colton.brenneman@gmail.com>
2023-06-29 10:15:17 -0600

    basic view setup complete

---
+++ AnimeMVVM/AnimeMVVM/UI/CharacterTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CharacterTableViewCell.swift
+//  AnimeMVVM
+//
+//  Created by Colton Brenneman on 6/29/23.
+//
+
+import UIKit
+
+class CharacterTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
